0c77bbd358bdf822fd56e9c74d06c83e31591b36
Austin Wasson <austinpwasson@gmail.com>
2024-01-12 16:50:06 -0700

    bug fix

--- app/controllers/reporterr/errors_controller.rb
+++ app/controllers/reporterr/errors_controller.rb
@@ -2,7 +2,7 @@ module Reporterr
   class ErrorsController < ApplicationController
     def index
       error_models = Reporterr.error_models
-      @errors = error_models.first.constantize.first(10)
+      @errors = error_models.empty? ? [] : error_models.first.constantize.first(10)
     end
   end
 end
