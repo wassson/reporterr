89439f6f849e0fb4e7450632638e91cb95eaac0d
Austin Wasson <austinpwasson@gmail.com>
2024-01-12 00:03:55 -0700

    table template

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        'lato': ['Lato', ...defaultTheme.fontFamily.sans]
       },
     },
   },
--- lib/reporterr/engine.rb
+++ lib/reporterr/engine.rb
@@ -5,6 +5,7 @@ module Reporterr
     # NOTE: add engine manifest to precompile assets in production, if you don't have this yet.
     initializer "my-engine.assets" do |app|
       app.config.assets.precompile += %w[reporterr_manifest]
+      app.config.assets.paths << Rails.root.join("app", "assets", "fonts")
     end
   end
 end
